56b27b72042060b7bf41ea1b66995e1f2c9a5ae9
Jonathan Portillo <jonathanportillo@MacBook-Pro.home>
2020-08-19 23:11:22 -0400

    routes Navjs and imports added

--- subreddit-predictor/src/App.js
+++ subreddit-predictor/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Link, Route, Switch } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import Landing from "./Components/Landing";
+import Login from "./Components/Login";
+import Nav from "./Components/Nav";
+import Signup from "./Components/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SubReddit Predictor</h1>
+      <h3>We'll Tell You Where Your Post belongs</h3>
     </div>
   );
 }
